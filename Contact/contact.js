8cf9c68c4562636f0b23ef97759b217b4dd49fee
kima meliane <melianekima8@gmail.com>
2026-07-24 20:17:35 +0000

    Mise à jour de ma page de contact

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,1 +1,29 @@
+const form = document.getElementById("contact-form");
+const status = document.getElementById("form-status");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const nom = document.getElementById("nom").value.trim();
+    const email = document.getElementById("email").value.trim() ;
+    const message = document.getElementById("message").value.trim();
+    if (nom === "" || email === "" || message === "") {
+        status.textContent = "Veuillez remplir tous les champs obligatoires";
+        status.style.color = "red";
+        return;
+    }
+    status.textContent = " ✅Votre message a bien été envoyé. Nous vous répondrons rapidement.";
+   status.style.color="#2e7d32";
+    form.reset();
+});
+setTimeout(() => {
+    status.textContent = "";
+},5000);
 
+const btnCommander = document.getElementById('btn-commander');
+const cartCount = document.getElementById('cart-count');
+
+let compteur = 0;
+
+btnCommander.addEventListener('click', () => {
+  compteur++;
+  cartCount.textContent = compteur; 
+});
